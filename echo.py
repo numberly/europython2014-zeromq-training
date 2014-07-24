ab4f5d0c2f849a599df08c77059a237119b5b629
Nicolas Delaby <nicolas.delaby@ezeep.com>
2014-07-24 10:13:38 +0200

    first echo server based on ROUTER and zmqstream

---
+++ echo.py
@@ -0,0 +1,20 @@
+import zmq
+from zmq.eventloop import ioloop, zmqstream
+
+
+io_loop = ioloop.IOLoop()
+
+context = zmq.Context()
+
+socket = context.socket(zmq.ROUTER)
+
+
+stream = zmqstream.ZMQStream(socket, io_loop=io_loop)
+
+
+def echo(stream, message):
+    stream.send_multipart(message)
+
+stream.on_recv_stream(echo)
+
+io_loop.start()
