c945da81c0199a8c04a9e5315ed6c4528dd4be24
Tim Czerniak <tim@demonware.net>
2014-07-24 11:38:49 +0200

    pink broker, improved from original

---
+++ pink/broker.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import argparse
+import zmq
+from zmq.eventloop import ioloop, zmqstream
+import json
+
+
+io_loop = ioloop.IOLoop()
+
+context = zmq.Context()
+
+socket = context.socket(zmq.ROUTER)
+
+
+stream = zmqstream.ZMQStream(socket, io_loop=io_loop)
+
+CLIENTS = set()
+
+
+def hello(stream, message):
+    print message
+    CLIENTS.add(message[1])
+    reply = [message[0]] + list(CLIENTS)
+    stream.send_multipart(reply)
+
+stream.on_recv_stream(hello)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--bind-address', default='tcp://0.0.0.0:5555')
+
+args = parser.parse_args()
+
+socket.bind(args.bind_address)
+io_loop.start()
