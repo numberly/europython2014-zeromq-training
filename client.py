8529f5f4e1d1ed295f415e191f7ca2ab10d7fb37
Alexandre M. S <alexsavio@gmail.com>
2014-07-24 10:44:50 +0200

    Add config file with SERVER_ADDRESS

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import zmq
+from config import SERVER_ADDRESS
 
 context = zmq.Context()
 
 socket = context.socket(zmq.DEALER)
 
-socket.connect('tcp://127.0.0.1:5555')
+socket.connect('tcp://{}'.format(SERVER_ADDRESS))
 for i in range(10):
     socket.send(str(i))
     print socket.recv()
