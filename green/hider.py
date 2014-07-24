cf6aaa0118366d930e34b7e7cf126c0672c9c774
Emil H <emil.hellman@gmail.com>
2014-07-24 12:24:07 +0200

    Simple hider

---
+++ green/hider.py
@@ -0,0 +1,34 @@
+
+import argparse
+import zmq
+from zmq.eventloop import ioloop, zmqstream
+
+io_loop = ioloop.IOLoop()
+
+context = zmq.Context()
+
+socket = context.socket(zmq.ROUTER)
+
+stream = zmqstream.ZMQStream(socket, io_loop=io_loop)
+
+
+
+
+def check_guess(stream, message):
+    addr, guess = message
+    response = "CORRECT" if guess in CITIES else "INCORRECT"
+    stream.send_multipart((addr, response))
+
+
+stream.on_recv_stream(check_guess)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--bind-address', default='tcp://0.0.0.0:5556')
+parser.add_argument('-c', '--cities', default='Berlin')
+
+args = parser.parse_args()
+
+CITIES = set(args.cities.split(","))
+
+socket.bind(args.bind_address)
+io_loop.start()
