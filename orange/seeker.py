ce2a314c888593121e047a7506bebe78ec16c536
Blaz Bratanic <blaz.bratanic@sensum.eu>
2014-07-24 12:20:47 +0200

    Implemented seeker

--- orange/seeker.py
+++ orange/seeker.py
@@ -8,7 +8,8 @@ context = zmq.Context()
 socket = context.socket(zmq.DEALER)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
+#parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
+parser.add_argument('-c', '--connect-address', default='tcp://172.16.17.133:5555')
 parser.add_argument('-p', '--port', default=5556)
 
 args = parser.parse_args()
@@ -21,4 +22,17 @@ socket.connect(args.connect_address)
 # First just register to the server
 command = '{}:{}'.format(myip, args.port)
 socket.send(command)
-print(socket.recv_string())
+players = socket.recv_string().split()
+
+guesses = ['Ljubljana', 'Stockholm']
+
+for player in players:
+    player_socket = context.socket(zmq.DEALER)
+    player_socket.connect('tcp://{}'.format(player))
+    for guess in guesses:
+        player_socket.send(guess)
+        if socket.recv_string() == 'yes':
+            print('Guessed: {}'.format(guess))
+            break
+    else:
+        print("Couldn't guess the answer")
