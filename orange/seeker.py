47fd377fdeb820ccc63537c7208b41cf8ebd63c8
Frank Lazzarini <flazzarini@gmail.com>
2014-07-24 12:18:23 +0200

    Merged

--- orange/seeker.py
+++ orange/seeker.py
@@ -9,11 +9,7 @@ socket = context.socket(zmq.DEALER)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
-<<<<<<< HEAD
 parser.add_argument('-p', '--port', default=5555)
-=======
-parser.add_argument('-p', '--port', default=5556)
->>>>>>> upstream/master
 
 args = parser.parse_args()
 
@@ -25,9 +21,5 @@ socket.connect(args.connect_address)
 # First just register to the server
 command = '{}:{}'.format(myip, args.port)
 socket.send(command)
-<<<<<<< HEAD
 resp = socket.recv()
 print resp
-=======
-print(socket.recv_string())
->>>>>>> upstream/master
