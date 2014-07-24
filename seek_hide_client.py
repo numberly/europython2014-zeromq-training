dbee79298a0cc77d0fcddd5e4834ea4db9b78017
Bartosz Telenczuk <muchatel@poczta.fm>
2014-07-24 11:01:44 +0200

    added message with ip

--- seek_hide_client.py
+++ seek_hide_client.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import argparse
 import zmq
+import socket
+import json
+ip = socket.gethostbyname(socket.AF_APPLETALKgethostname())
 
 context = zmq.Context()
 
@@ -11,7 +14,11 @@ parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
 
 args = parser.parse_args()
 
+msg = {'msg' : "hello",
+        "myip" : ip}
+
+
 socket.connect(args.connect_address)
 for i in range(10):
-    socket.send(str(i))
+    socket.send(json.dumps(msg))
     print socket.recv()
