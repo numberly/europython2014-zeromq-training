49849b9fab3e0f45f73e581b41e08dfbdd5628f4
Tim Czerniak <tim@demonware.net>
2014-07-24 12:57:56 +0200

    fully working hider for pink team

--- pink/hider.py
+++ pink/hider.py
@@ -9,28 +9,45 @@ io_loop = ioloop.IOLoop()
 
 context = zmq.Context()
 
-socket = context.socket(zmq.ROUTER)
 
-
-stream = zmqstream.ZMQStream(socket, io_loop=io_loop)
 
 CITY = None
 
 
+def get_ip():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('www.google.com', 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
 def guess(stream, message):
     print message
     city = message[1]
-    reply = "YES" if city == CITY else "NO"
+    reply = "CORRECT" if city == CITY else "INCORRECT"
     stream.send_multipart([message[0], reply])
 
-stream.on_recv_stream(guess)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--bind-address', default='tcp://0.0.0.0:5555')
+parser.add_argument('-p', '--port', default='5557')
+parser.add_argument('-C', '--connect-address', default='tcp://172.16.16.188:5556')
 parser.add_argument('-c', '--city', default='Berlin')
 
 args = parser.parse_args()
 
+# First just register to the server
+myip = get_ip()
+sock = context.socket(zmq.DEALER)
+sock.connect(args.connect_address)
+command = 'REGISTER {}:{}'.format(myip, args.port)
+sock.send_string(command)
+print sock.recv_multipart()
+
+# start server
 CITY = args.city
-socket.bind(args.bind_address)
+sock = context.socket(zmq.ROUTER)
+stream = zmqstream.ZMQStream(sock, io_loop=io_loop)
+stream.on_recv_stream(guess)
+sock.bind("tcp://%s:%s" %(myip, args.port))
 io_loop.start()
