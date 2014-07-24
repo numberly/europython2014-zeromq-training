8571b02c5d3dd8b15b6dcce7bebe2667948d1aac
Tim Czerniak <tim@demonware.net>
2014-07-24 10:41:36 +0200

    adding server address arg to client

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 import zmq
+import sys
+
+server_address = sys.argv[1]
 
 context = zmq.Context()
 
 socket = context.socket(zmq.DEALER)
 
-socket.connect('tcp://127.0.0.1:5555')
+socket.connect('tcp://%s:5555' % server_address)
 for i in range(10):
     socket.send(str(i))
     print socket.recv()
