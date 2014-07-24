e4c26c0d54a7d933d3d941c6020aea66918c9377
Wavilon <admin@wavilon.com>
2014-07-24 10:42:21 +0200

    Added --server command line flag

--- client.py
+++ client.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 import zmq
+import sys
+import getopt
 
 context = zmq.Context()
 
 socket = context.socket(zmq.DEALER)
 
-socket.connect('tcp://127.0.0.1:5555')
+SHORT_OPTIONS = 's:'
+
+LONG_OPTIONS = [ 'server=' ]
+
+opts, args = getopt.getopt(sys.argv[1:], SHORT_OPTIONS, LONG_OPTIONS)
+options = { 'server' : 'tcp://127.0.0.1:5555' }
+
+for o, a in opts:
+    if o in ('-s', '--server',):
+        options['server']  = a
+    else:
+        print 'ERR: option %s not supported' % (o)
+
+server = options['server']
+print 'Connecting to server %s ...' % (server)
+socket.connect(server)
+
 for i in range(10):
     socket.send(str(i))
     print socket.recv()
