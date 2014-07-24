b62a5d785055c03378d230d9ac159fe444c0d4c6
regadas <filiperegadas@gmail.com>
2014-07-24 10:45:26 +0200

    better client

--- client.py
+++ client.py
@@ -1,10 +1,32 @@
 import zmq
+from optparse import OptionParser
 
-context = zmq.Context()
 
-socket = context.socket(zmq.DEALER)
+class ZMQClient(object):
+    def __init__(self):
+        self.context = zmq.Context()
 
-socket.connect('tcp://127.0.0.1:5555')
-for i in range(10):
-    socket.send_multipart([str(i)])
-    print socket.recv()
+    def connect(self, addr):
+        self.addr = addr
+        self.socket = self.context.socket(zmq.DEALER)
+        self.socket.connect(self.addr)
+
+    def send(self, msg):
+        self.socket.send_multipart([str(i)])
+        return self.socket.recv()
+
+if __name__ == '__main__':
+    parser = OptionParser()
+
+    parser.add_option(
+        "-a",
+        "--address",
+        dest="address",
+        default='tcp://127.0.0.1:5555'
+    )
+    (options, args) = parser.parse_args()
+    client = ZMQClient()
+    client.connect(options.address)
+
+    for i in range(10):
+        print client.send(str(i))
