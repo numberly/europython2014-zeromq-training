b0a161ad487eeaf1f2e3b9b7fbe60c1a68a3dff6
Alexandre M. S <alexsavio@gmail.com>
2014-07-24 11:42:30 +0200

    Fix seek_hide_client get_local_ip function

--- yellow/seek_hide_client.py
+++ yellow/seek_hide_client.py
@@ -2,6 +2,16 @@
 import argparse
 import zmq
 
+def get_local_ip():
+    """
+    Retrieve the clients local ip address and return it as a string
+
+    :returns IpAddress as String
+    """
+    import socket
+    return socket.gethostbyname(socket.gethostname())
+
+
 context = zmq.Context()
 
 socket = context.socket(zmq.DEALER)
