c60ba6151559c82f14d7b6b72437bba4821fc773
Nicolas Delaby <nicolas.delaby@ezeep.com>
2014-07-24 10:24:03 +0200

    minimal client for echo server

---
+++ client.py
@@ -0,0 +1,10 @@
+import zmq
+
+context = zmq.Context()
+
+socket = context.socket(zmq.DEALER)
+
+socket.connect('tcp://127.0.0.1:5555')
+for i in range(10):
+    socket.send_multipart([str(i)])
+    print socket.recv()
