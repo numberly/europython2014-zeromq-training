5512266cbf60de2113b73d7683f8629692756042
Holger Joukl <holger.joukl@gmx.de>
2014-07-24 10:40:04 +0200

    Add argument parsing to client.py

--- client.py
+++ client.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 import zmq
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--connect', help='connection address (default: %(default)s)',
+    default='tcp://127.0.0.1:5555')
+
+args = parser.parse_args()
+
 
 context = zmq.Context()
 
 socket = context.socket(zmq.DEALER)
 
-socket.connect('tcp://127.0.0.1:5555')
+socket.connect(args.connect)
 for i in range(10):
     socket.send_multipart([str(i)])
     print socket.recv()
