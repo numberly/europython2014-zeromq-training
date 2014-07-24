0ad728882ae4b04558d2e6085eb895509ea3dca8
Bartosz Telenczuk <muchatel@poczta.fm>
2014-07-24 11:08:03 +0200

    updated message

--- seek_hide_client.py
+++ seek_hide_client.py
@@ -14,7 +14,7 @@ parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
 
 args = parser.parse_args()
 
-msg = "HELLO {ip} {port}".format(ip=ip, port=port) 
+msg = "HELLOFROM {ip} {port}".format(ip=ip, port=port) 
 socket.connect(args.connect_address)
 for i in range(10):
     socket.send(msg)
