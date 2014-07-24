003433c209eb7715134f0e8688b7a23c83197fd5
Achim Domma <domma@procoders.net>
2014-07-24 11:51:31 +0200

    basics working

--- black/seeker.py
+++ black/seeker.py
@@ -9,7 +9,8 @@ socket = context.socket(zmq.DEALER)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
-parser.add_argument('-p', '--port', default=4444)
+parser.add_argument('-p', '--port', default="4444")
+parser.add_argument('-i', '--ip')
 
 args = parser.parse_args()
 
@@ -20,5 +21,5 @@ socket.connect(args.connect_address)
 
 # First just register to the server
 command = 'HELLO {} {}'.format(myip, args.port)
-socket.send_multipart([myip, args.port])
+socket.send_multipart([args.ip, args.port])
 print socket.recv_json()
