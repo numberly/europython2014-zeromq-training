dcc3275691985df8cad3161945cbd057c4ce597b
Blaz Bratanic <blaz.bratanic@sensum.eu>
2014-07-24 11:08:53 +0200

    send my ip address

--- seek_hide_client.py
+++ seek_hide_client.py
@@ -23,6 +23,5 @@ parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
 args = parser.parse_args()
 
 socket.connect(args.connect_address)
-for i in range(10):
-    socket.send(str(i))
-    print socket.recv()
+socket.send(str(get_local_ip()))
+print socket.recv()
